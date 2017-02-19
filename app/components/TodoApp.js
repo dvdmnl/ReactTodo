3fdf215f041f203359b4ad09ab5178e2734c6ea2
David Manela <dudimanela@gmail.com>
2017-02-19 20:00:50 +0200

    Add TodoApp.js

---
+++ app/components/TodoApp.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import uuid from 'node-uuid'
+import moment from 'moment'
+
+import TodoList from 'TodoList'
+import AddTodo from 'AddTodo'
+import TodoSearch from 'TodoSearch'
+
+
+class TodoApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1 className="page-title">Todo App</h1>
+                <div className="row">
+                    <div className="column small-centered small-11 medium-6 large-5">
+                        <div className="container">
+                            <TodoSearch/>
+                            <TodoList/>
+                            <AddTodo/>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default TodoApp;
